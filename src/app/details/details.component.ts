f7be36d7ce7aceb76438947343d1a76d1177bf9e
anesushangwa01 <anesushangwa01@gmail.com>
2024-01-25 13:07:22 +0200

    new
    new

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,7 +2,7 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
-import { HousingService } from '../housing.service';
+import { HousingService } from '../housing';
 import { HousingLocation } from '../housinglocation';
 import { FormControl, FormGroup, ReactiveFormsModule,  Validators } from '@angular/forms';
 
